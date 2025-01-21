defb61fb0853a2613cda3b8a7fa437ae4f8cbf5c
Leon David Zipp <leondavidzipp@gmx.de>
2025-01-21 22:37:07 +0100

    fleshed out forwarder_service_impl.go: added variables + forward implementation

--- src/api/forwarder_service_impl.go
+++ src/api/forwarder_service_impl.go
@@ -1,17 +1,29 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	f "github.com/LeonDavidZipp/StackITCloudAcceleratorCodingChallenge/src/api/gen/forwarder_service"
+	c "github.com/LeonDavidZipp/StackITCloudAcceleratorCodingChallenge/src/config"
+	"net/http"
 )
 
 // ForwarderService implements the forwarder service
-type ForwarderService struct{}
+type ForwarderService struct {
+	client *http.Client
+	url    string
+	chatID string
+}
 
 // NewForwarderService creates a new forwarder service
-func NewForwarderService() *ForwarderService {
-	return &ForwarderService{}
+func NewForwarderService(id string) *ForwarderService {
+	return &ForwarderService{
+		client: &http.Client{},
+		url:    "https://api.telegram.org/bot" + c.TELEGRAM_BOT_TOKEN,
+		chatID: id,
+	}
 }
 
 // Forward forwards a notification if it is a warning, otherwise it ignores it
@@ -19,17 +31,54 @@ func (s *ForwarderService) Forward(ctx context.Context, m *f.Notification) (err 
 	// switch case for future extensibility
 	switch m.Type {
 	case "Warning":
-		// TODO: Forward to the appropriate channel
-		fmt.Println("sending to channel")
+		if err := s.forward(ctx, m); err != nil {
+			return err
+		}
 	default:
-		//TODO:  ignore
-		fmt.Println("ignoring")
+		break
 	}
 	return nil
 }
 
+// forwardRequestBody defines the request body for the forward method of the telegram bot api
+type forwardRequestBody struct {
+	chatID string
+	text   string
+}
+
 // forward forwards a warning to the appropriate channel
 func (s *ForwarderService) forward(ctx context.Context, m *f.Notification) (err error) {
-	//TODO: implement
+	body, err := json.Marshal(
+		forwardRequestBody{
+			chatID: s.chatID,
+			text:   "Warning\n" + m.Description,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		"POST",
+		s.url+"/sendMessage",
+		bytes.NewBuffer(body),
+	)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
 	return nil
 }
